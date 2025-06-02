696cdb9b847dba2b1653230cd4699dec2d36cc3a
Stuart Pethurst <stuart.pethurst@colossusbets.com>
2025-06-02 21:04:06 +0100

    updated routes for contacts/new

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,19 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "privacy", to: "pages#privacy", as: :privacy
 
-  # Only allow POST to create contacts
+  # Contact form
+  get "/contact", to: "contacts#new", as: :contact
   resources :contacts, only: [:new, :create]
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # PWA files (optional, safe to leave)
+  # PWA files
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   namespace :admin do
     get "contacts/index"
-    resources :contacts, only: [ :index ]
+    resources :contacts, only: [:index]
   end
 end
