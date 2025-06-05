97875a013dffb78847badcf18197ddaf7be5be6f
Stuart Pethurst <stuart.pethurst@colossusbets.com>
2025-06-05 20:53:49 +0100

    fixed allow_browser versions: {   safari: 15..,   chrome: 100..,   firefox: 100..,   edge: 100..,   ios_safari: 15..,   samsung_internet: 15.. }

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,5 @@ class ApplicationController < ActionController::Base
   end
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  allow_browser versions: :all
 end
