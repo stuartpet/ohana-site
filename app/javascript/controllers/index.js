466f7fbc677af5d0e887e172aa24d8dfdb2b4f5b
Stuart Pethurst <stuart.pethurst@colossusbets.com>
2025-06-08 09:49:41 +0100

    made the videos mobile responsive

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import CookieBannerController from "./cookie_banner_controller"
+import MenuController from "./menu_controller"
+import Scroll_controller from "./scroll_controller";
 
 const application = Application.start()
 application.register("cookie-banner", CookieBannerController)
+application.register("menu", MenuController)
+application.register("scroll", Scroll_controller)
 // register others...
