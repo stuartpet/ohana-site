2281afcdfff57ada01294febe31437814e6464c9
Stuart Pethurst <stuart.pethurst@colossusbets.com>
2025-06-08 20:16:52 +0100

    update scss removed some duplicates

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,31 +1,32 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["menu", "button", "overlay"]
+    static targets = ["menu", "button", "overlay"];
 
     toggle() {
-        this.menuTarget.classList.toggle("open")
-        this.buttonTarget.classList.toggle("open")
-        this.overlayTarget.classList.toggle("open")
-        document.body.classList.toggle("menu-open")
+        this.menuTarget.classList.toggle("open");
+        this.buttonTarget.classList.toggle("open");
+        this.overlayTarget.classList.toggle("open");
+        document.body.classList.toggle("menu-open");
 
-        if (window.innerWidth <= 768) {
-            const links = this.menuTarget.querySelectorAll("a")
-            if (this.menuTarget.classList.contains("open")) {
-                links.forEach((link, i) =>
-                    setTimeout(() => {
-                        link.classList.remove("pack")
-                        link.classList.add("unpack")
-                    }, i * 100)
-                )
-            } else {
-                links.forEach((link, i) =>
-                    setTimeout(() => {
-                        link.classList.remove("unpack")
-                        link.classList.add("pack")
-                    }, i * 50)
-                )
-            }
+        const isMobile = window.innerWidth <= 768;
+        const links = this.menuTarget.querySelectorAll("a");
+        if (!isMobile) return;
+
+        if (this.menuTarget.classList.contains("open")) {
+            links.forEach((link, i) =>
+                setTimeout(() => {
+                    link.classList.remove("pack");
+                    link.classList.add("unpack");
+                }, i * 100)
+            );
+        } else {
+            links.forEach((link, i) =>
+                setTimeout(() => {
+                    link.classList.remove("unpack");
+                    link.classList.add("pack");
+                }, i * 50)
+            );
         }
     }
 }
