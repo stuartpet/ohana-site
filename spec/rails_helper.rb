c06d95d5dff8ef4b9dfb1a1e01cb0a3cc6ef71ad
Stuart Pethurst <stuart.pethurst@colossusbets.com>
2025-07-03 12:19:36 +0100

    fix failing specs due to no activerecord

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,11 +31,11 @@ Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 # If there are pending migrations it will invoke `db:test:prepare` to
 # recreate the test database by loading the schema.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
-end
+# begin
+#   ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
+#   abort e.to_s.strip
+# end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
